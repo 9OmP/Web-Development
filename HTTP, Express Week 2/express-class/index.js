5bfba1240394a362f04e0dbe17d0c8a784ad3a5b
9OmP <you@example.com>
2024-09-13 09:08:22 +0530

    completed express class

--- HTTP, Express Week 2/express-class/index.js
+++ HTTP, Express Week 2/express-class/index.js
@@ -1,20 +1,91 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-function sum(n){
-    let ans = 0;
-    for (let i = 0; i <= n; i++) {
-        ans += i;
-    }
-    return ans;
+const user = {
+    name: "om",
+    kidneys: [{
+        healthy: true
+    }]
 }
 
+const users = [user] 
+
 app.get('/', function(req, res) {
-    const n = req.query.n;
-    const ans = sum(n);
-    res.send(`hi there, your sum is ${ans}`);
+    let user_kidneys = users[0].kidneys;
+    let num_of_kidneys = user_kidneys.length;
+    let num_of_healthy_kidneys = 0;
+    for (let i = 0; i < user_kidneys.length; i++) {
+        if (user_kidneys[i].healthy === true) {
+            num_of_healthy_kidneys++;
+        }
+    }
+    let num_of_unhealthy_kidneys = num_of_kidneys - num_of_healthy_kidneys;
+
+    res.json({
+        name: users[0].name,
+        num_of_kidneys: num_of_kidneys,
+        num_of_healthy_kidneys: num_of_healthy_kidneys,
+        num_of_unhealthy_kidneys: num_of_unhealthy_kidneys
+    });
 })
+
+app.post("/", function(req, res) {
+    const is_healthy = req.body.is_healthy;
+    users[0].kidneys.push({
+        healthy: is_healthy
+    });
+    res.json({
+        message: "kidney added"
+        });
+});
+
+
+app.put("/", function(req, res) {
+    for(let i = 0; i < users[0].kidneys.length; i++){
+        users[0].kidneys[i].healthy = true;
+    }
+    res.json({
+        msg: "kidneys updated"
+    })
+});
+
+app.delete("/", function(req, res) {
+    if (is_there_atleast_one_unhealthy_kidney()){
+        const new_kidneys = [];
+    for(let i = 0; i < users[0].kidneys.length; i++){
+        if (users[0].kidneys[i].healthy){
+            new_kidneys.push(
+            users[0].kidneys[i]
+        );
+      }
+   }
+   users[0].kidneys = new_kidneys;
+   res.json(
+      {
+        message: "unhealthy kidneys deleted"
+      }
+    );
+    } else{
+        res.status(411).json(
+            {
+                message: "no unhealthy kidneys to delete"
+            }
+    );
+    }
+    
+});
+
+function is_there_atleast_one_unhealthy_kidney(){
+    for(let i = 0; i < users[0].kidneys.length; i++){
+        if (users[0].kidneys[i].healthy === false){
+            return true;
+        }
+    }
+    return false;
+}
+
 
 app.listen(3000);
 
